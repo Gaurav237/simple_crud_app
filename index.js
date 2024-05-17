feadb0e66e53ebdc1b522c7ba38cc4ffdbe20ea6
Gaurav237 <gktiwary237@gmail.com>
2024-05-17 21:26:05 +0530

    express installed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello");
+const express = require('express')
+const app = express()
 
+app.listen(3000, () => {
+    console.log(`Server is listening on port: 3000`)
+})
+
+app.get('/', (req, res) => {
+    res.send("HELLO form NODE API")
+})
