842dd52aee007932f0ec552ad7c8e3559ede11b6
Gaurav237 <gktiwary237@gmail.com>
2024-05-18 00:02:29 +0530

    mongodb and mongoose installed

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, () => {
-    console.log(`Server is listening on port: 3000`)
-})
 
 app.get('/', (req, res) => {
     res.send("HELLO form NODE API")
 })
+
+mongoose.connect('mongodb+srv://gaurav237:9234valorant@backenddb.9a8h9ty.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
+    .then(() => {
+        console.log("Connected to databse!")
+        app.listen(3000, () => {
+            console.log(`Server is listening on port: 3000`)
+        })
+    })
+    .catch(() => {
+        console.log('Connection failed!')
+    })
