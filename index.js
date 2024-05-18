4a1aa7bd2e16579814afc70a59ed1846a71468b7
Gaurav237 <gktiwary237@gmail.com>
2024-05-18 16:13:17 +0530

    typo removed

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ app.get('/api/products', async (req, res) => {
     }
 })
 
-app.get('/api/products', async (req, res) => {
+app.get('/api/products/:id', async (req, res) => {
     try{
-        const products = await Product.find({})
+        const id = req.params.id
+        const products = await Product.findById(id)
         res.status(200).json(products)
     }catch(err){
         res.status(500).json({message: err.message})
     }
 })
 
-app.get('/api/products/:id', async (req, res) => {
+app.post('/api/products', async (req, res) => {
     try{
-        const id = req.params.id
-        const products = await Product.findById(id)
-        res.status(200).json(products)
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
     }catch(err){
         res.status(500).json({message: err.message})
     }
