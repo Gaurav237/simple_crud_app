e1502b47d08c3d573ab66e18535edb75d2c412c6
Gaurav237 <gktiwary237@gmail.com>
2024-05-18 15:52:48 +0530

    find product api added

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ app.get('/', (req, res) => {
     res.send("HELLO form NODE API")
 })
 
-app.post('/api/products', async (req, res) => {
+app.get('/api/products', async (req, res) => {
     try{
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
+        const products = await Product.find({})
+        res.status(200).json(products)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+app.get('/api/products', async (req, res) => {
+    try{
+        const products = await Product.find({})
+        res.status(200).json(products)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+app.get('/api/products/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const products = await Product.findById(id)
+        res.status(200).json(products)
     }catch(err){
         res.status(500).json({message: err.message})
     }
