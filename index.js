be3cf111de9fb2109da03e45d90b0d0795413238
Gaurav237 <gktiwary237@gmail.com>
2024-05-18 16:40:34 +0530

    update product api added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/api/products', async (req, res) => {
     }
 })
 
-app.get('/api/products/:id', async (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
     try{
         const id = req.params.id
         const products = await Product.findById(id)
@@ -31,6 +31,20 @@ app.get('/api/products/:id', async (req, res) => {
 app.post('/api/products', async (req, res) => {
     try{
         const product = await Product.create(req.body)
+        res.status(200).json(product)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+// update a product
+app.put('/api/product/:id', async(req, res) => {
+    try{
+        const { id } = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body, {new: true})
+        if(!product){
+            res.status(404).json({message: "Product not found"})
+        }
         res.status(200).json(product)
     }catch(err){
         res.status(500).json({message: err.message})
